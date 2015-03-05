b76d274ca595255320b9b4503831c8a36004e599
Brandon Duff <brandonduff17@gmail.com>
2015-03-05 14:50:56 -0700

    Starting to work on transitions. Pushing so I can work remotely (if I get the chance).

---
+++ scripts/transition.js
@@ -0,0 +1,16 @@
+CarGame.screens['transition'] = (function() {
+
+    function initialize(){
+        //TODO: Figure out if we need to initialize anything and write our run
+    }
+
+    function run() {
+
+    }
+
+    return {
+        initialize : initialize,
+        run : run
+    }
+
+}());
